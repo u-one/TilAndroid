b682426ff8766ba1986f237b8f6e77edc864da92
u-one <bacflip270bb@gmail.com>
2023-01-05 19:24:37 +0900

    layout and material design

--- app/src/main/java/net/uoneweb/android/til/ui/main/MainFragment.kt
+++ app/src/main/java/net/uoneweb/android/til/ui/main/MainFragment.kt
@@ -1,17 +1,29 @@
 package net.uoneweb.android.til.ui.main
 
+import android.content.res.Configuration
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.uoneweb.android.til.R
+import net.uoneweb.android.til.ui.theme.MyApplicationTheme
 
 class MainFragment : Fragment() {
 
@@ -34,20 +46,63 @@ class MainFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-                MessageCard(name = "Main")
+                MyApplicationTheme {
+                    Surface(modifier = Modifier.fillMaxSize()) {
+                        MessageCard(Message("Android", "MainFragment") )
+                    }
+                }
             }
         }
     }
 
+    data class Message(val author: String, val body: String)
+
     @Composable
-    fun MessageCard(name: String) {
-        Text(text = "${name}Fragment")
+    fun MessageCard(msg: Message) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "icon",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+
+                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                    Text(
+                        text = msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.body2
+                    )
+                }
+            }           
+        }
     }
 
-    @Preview
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
     @Composable
-    fun MessageCardPreview() {
-        MessageCard("Main")
+    fun PreviewMessageCard() {
+        MyApplicationTheme {
+            Surface {
+                MessageCard(Message("test1", "test2"))
+            }
+        }
     }
 
 }

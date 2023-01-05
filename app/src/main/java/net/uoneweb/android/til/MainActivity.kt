bb0936aaab9c874273a1fe678b92bc58fab276e8
u-one <bacflip270bb@gmail.com>
2023-01-05 16:46:16 +0900

    initial files

---
+++ app/src/main/java/net/uoneweb/android/til/MainActivity.kt
@@ -0,0 +1,18 @@
+package net.uoneweb.android.til
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.uoneweb.android.til.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/net/uoneweb/android/til/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package net.uoneweb.android.til.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

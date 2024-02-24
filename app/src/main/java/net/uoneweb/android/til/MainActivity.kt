80734650fc55802ea563ebf124e5747f301903b6
u-one <backflip270bb@gmail.com>
2024-02-24 14:32:17 +0900

    Add BottomNavigation

--- app/src/main/java/net/uoneweb/android/til/MainActivity.kt
+++ app/src/main/java/net/uoneweb/android/til/MainActivity.kt
@@ -2,25 +2,42 @@ package net.uoneweb.android.til
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.drawable.Icon
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import net.uoneweb.android.til.ui.camera.CameraFragment
@@ -84,14 +101,54 @@ class MainActivity : AppCompatActivity() {
 @Composable
 private fun TilApp() {
     val navController = rememberNavController()
-    TilNavHost(navController = navController)
+    Scaffold(bottomBar = {
+        BottomBar(navController)
+    }) {innerPadding ->
+        TilNavHost(navController = navController, modifier = Modifier.padding(innerPadding))
+    }
+}
+
+sealed class Screen(val route: String, @StringRes val resourceId: Int, val icon: ImageVector) {
+    object Main : Screen("main", R.string.main, Icons.Filled.Home)
+    object Camera : Screen("camera", R.string.camera, Icons.Filled.AccountBox)
+}
+
+private val screens = listOf(
+    Screen.Main,
+    Screen.Camera
+)
+
+@Composable
+private fun BottomBar(navController: NavController) {
+    BottomNavigation {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+        screens.forEach {screen ->
+            BottomNavigationItem(
+                icon = { Icon(screen.icon, contentDescription = stringResource(screen.resourceId)) },
+                label = { Text(stringResource(screen.resourceId)) },
+                selected = currentDestination?.hierarchy?.any {it.route == screen.route } == true,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+
+    }
 }
 
 @Composable
 private fun TilNavHost(
-    navController: NavHostController
+    navController: NavHostController,
+    modifier: Modifier
 ) {
-    NavHost(navController = navController, startDestination = "main") {
+    NavHost(navController = navController, startDestination = "main", modifier = modifier) {
         composable("camera") {
             CameraFragmentComposable()
         }

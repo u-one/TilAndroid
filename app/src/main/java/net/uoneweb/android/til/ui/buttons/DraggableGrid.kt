6fd60b268fbf292041ccbcaa95c7dd290d16f4be
u-one <backflip270bb@gmail.com>
2024-03-17 14:59:50 +0900

    Create DraggableGridState

--- app/src/main/java/net/uoneweb/android/til/ui/buttons/DraggableGrid.kt
+++ app/src/main/java/net/uoneweb/android/til/ui/buttons/DraggableGrid.kt
@@ -1,6 +1,5 @@
 package net.uoneweb.android.til.ui.buttons
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
@@ -18,10 +17,8 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -36,154 +33,36 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toOffset
 import androidx.compose.ui.unit.toSize
 
-@Composable
-fun DraggableGrid() {
-    val list = listOf(
-        "A",
-        "B",
-        "C",
-        "D",
-        "E",
-        "F",
-        "G",
-        "H",
-        "I",
-        "J",
-        "K",
-        "L",
-        "M",
-        "N",
-        "O",
-        "P",
-        "Q",
-        "R",
-        "S",
-        "T",
-        "U",
-        "V",
-        "W",
-        "X",
-        "Y",
-        "Z"
-    )
-
-
-    var draggingIndex by remember { mutableStateOf(-1) }
-    var offset by remember {
-        mutableStateOf(Offset(0f, 0f))
-    }
-
-    val lazyGridState = rememberLazyGridState()
-    val draggingItem = remember(lazyGridState, draggingIndex) {
-        lazyGridState.layoutInfo.visibleItemsInfo.find { info ->
+@Stable
+data class DraggableGridState(
+    var draggingIndex: Int = -1,
+    var offset: Offset = Offset.Zero,
+    val lazyGridState: LazyGridState
+) {
+    fun draggingItem(): LazyGridItemInfo? {
+        return lazyGridState.layoutInfo.visibleItemsInfo.find { info ->
             info.index == draggingIndex
         }
     }
 
-    val currentItemUnderDragIndex = rememberCurrentItemOnOffset(
-        lazyGridState = lazyGridState,
-        offset = offset,
-        draggingIndex = draggingIndex,
-        draggingItem = draggingItem
-    )
-
-    val temp = rememberCurrentItemsUnderOffset(
-        lazyGridState = lazyGridState,
-        offset = offset,
-        draggingIndex = draggingIndex,
-        draggingItem = draggingItem
-    )
-
-
-    Column {
-
-        LazyVerticalGrid(columns = GridCells.Adaptive(64.dp), state = lazyGridState) {
-            itemsIndexed(list) { index, item ->
-                ItemButton(
-                    item,
-                    Modifier
-                        .pointerInput(Unit) {
-                            detectDragGestures(
-                                onDragStart = { draggingIndex = index },
-                                onDragEnd = {
-                                    draggingIndex = -1
-                                    offset = Offset(0f, 0f)
-                                },
-                                onDragCancel = {
-                                    draggingIndex = -1
-                                    offset = Offset(0f, 0f)
-                                },
-                                onDrag = { change, dragAmount ->
-                                    offset += dragAmount
-                                    change.consume()
-                                }
-                            )
-                        },
-                    isDragging = (draggingIndex == index),
-                    offset = offset,
-                    onClickItem = {
-                        Log.d(
-                            "Button",
-                            "totalItemsCount: ${lazyGridState.layoutInfo.totalItemsCount} " +
-                                    "afterContentPadding: ${lazyGridState.layoutInfo.afterContentPadding} " +
-                                    "beforeContentPadding: ${lazyGridState.layoutInfo.beforeContentPadding} "
-                        )
-
-                        lazyGridState.layoutInfo.visibleItemsInfo.forEach { info ->
-                            //infoのすべてのプロパティをログに出力
-                            Log.d(
-                                "Button",
-                                "offset: ${info.offset} size: ${info.size} index:${info.index} row:${info.row} column:${info.column} ke:${info.key}"
-                            )
-                        }
-                    }
-                )
-            }
-        }
-        Text(
-            style = MaterialTheme.typography.body1, text =
-            "currentItemUnderDrag: $currentItemUnderDragIndex"
-        )
-        Text(
-            style = MaterialTheme.typography.body1, text =
-            "currentItemsUnderDrag: $temp"
-        )
-    }
-}
-
-@Composable
-fun rememberCurrentItemOnOffset(
-    lazyGridState: LazyGridState,
-    offset: Offset,
-    draggingIndex: Int,
-    draggingItem: LazyGridItemInfo?
-): Int {
-    return remember(lazyGridState, offset, draggingIndex) {
-        val center = draggingItem?.size?.center ?: IntOffset.Zero
+    fun currentItemUnderOffset(): Int {
+        val center = draggingItem()?.size?.center ?: IntOffset.Zero
         val offsetCenter = Offset(offset.x + center.x.toFloat(), offset.y + center.y.toFloat())
         val currentItem = lazyGridState.layoutInfo.visibleItemsInfo.find { info ->
             val rect = Rect(info.offset.toOffset(), info.size.toSize())
             rect.contains(offsetCenter)
         }
-        currentItem?.index ?: -1
+        return currentItem?.index ?: -1
     }
-}
 
-@Composable
-fun rememberCurrentItemsUnderOffset(
-    lazyGridState: LazyGridState,
-    offset: Offset,
-    draggingIndex: Int,
-    draggingItem: LazyGridItemInfo?
-): Pair<Int, Int>? {
-    return remember(lazyGridState, offset, draggingIndex) {
-        val center = draggingItem?.size?.center ?: IntOffset.Zero
+    fun currentItemsUnderOffset(): Pair<Int, Int>? {
+        val center = draggingItem()?.size?.center ?: IntOffset.Zero
         val offsetCenter = Offset(offset.x + center.x.toFloat(), offset.y + center.y.toFloat())
         val currentItem = lazyGridState.layoutInfo.visibleItemsInfo.find { info ->
             val rect = Rect(info.offset.toOffset(), info.size.toSize())
             rect.contains(offsetCenter)
         }
-        currentItem?.let {
+        return currentItem?.let {
             val currentItemCenter =
                 Rect(currentItem.offset.toOffset(), currentItem.size.toSize()).center
             if (offsetCenter.x > currentItemCenter.x) {
@@ -193,7 +72,82 @@ fun rememberCurrentItemsUnderOffset(
             }
         }
     }
+
+    fun onDraggingIndexChange(index: Int) {
+        draggingIndex = index
+    }
+
+    fun resetIndex() {
+        draggingIndex = -1
+        offset = Offset.Zero
+    }
+
+    fun onDrag(dragAmount: Offset) {
+        offset += dragAmount
+    }
+
 }
+
+@Composable
+fun rememberDraggableGridState(): DraggableGridState {
+    val lazyGridState = rememberLazyGridState()
+    return remember {
+        DraggableGridState(
+            lazyGridState = lazyGridState
+        )
+    }
+}
+
+@Composable
+fun DraggableGrid() {
+    // A list of characters from A to Z
+    val list = ('A'..'Z').map { it.toString() }
+
+    val draggableGridState = rememberDraggableGridState()
+
+    Column {
+
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(64.dp),
+            state = draggableGridState.lazyGridState
+        ) {
+            itemsIndexed(list) { index, item ->
+                ItemButton(
+                    item,
+                    Modifier
+                        .pointerInput(Unit) {
+                            detectDragGestures(
+                                onDragStart = { draggableGridState.onDraggingIndexChange(index) },
+                                onDragEnd = {
+                                    draggableGridState.resetIndex()
+                                },
+                                onDragCancel = {
+                                    draggableGridState.resetIndex()
+                                },
+                                onDrag = { change, dragAmount ->
+                                    draggableGridState.onDrag(dragAmount)
+                                    change.consume()
+                                }
+                            )
+                        },
+                    isDragging = (draggableGridState.draggingIndex == index),
+                    offset = draggableGridState.offset,
+                    onClickItem = {
+                    }
+                )
+            }
+        }
+        Text(
+            style = MaterialTheme.typography.body1, text =
+            "currentItemUnderDrag: ${draggableGridState.currentItemUnderOffset()}"
+        )
+        Text(
+            style = MaterialTheme.typography.body1, text =
+            "currentItemsUnderDrag: ${draggableGridState.currentItemsUnderOffset()}"
+        )
+    }
+}
+
 
 @Composable
 private fun ItemButton(
@@ -230,6 +184,7 @@ private fun ItemButton(
 
     }
 }
+
 
 @Preview(
     showBackground = true,

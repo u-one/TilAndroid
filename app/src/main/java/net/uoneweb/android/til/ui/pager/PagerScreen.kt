f901d6ab0fde01d00a9d3eb46b06a12b1c6aec97
u-one <backflip270bb@gmail.com>
2024-04-21 11:41:05 +0900

    Display characters

--- app/src/main/java/net/uoneweb/android/til/ui/pager/PagerScreen.kt
+++ app/src/main/java/net/uoneweb/android/til/ui/pager/PagerScreen.kt
@@ -4,11 +4,11 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.Stable
-import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -17,20 +17,22 @@ import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.geometry.toRect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun PagerScreen() {
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        PagerLcd()
+        PagerLcd(Modifier.height(160.dp))
     }
 }
 
 @Stable
-class PagerLcdState(val dotMatrixLcdState: State<DotMatrixLcdState>) {
-    val ch = PagerChar.Emoji(PagerCode("1*2"))
+class PagerLcdState(val dotMatrixLcdState: DotMatrixLcdState) {
+    val emSmile = PagerChar.Emoji(PagerCode("1*2"))
     val chA = PagerChar.Alpha(
         PagerCode("16"), "A",
         """
@@ -44,37 +46,123 @@ class PagerLcdState(val dotMatrixLcdState: State<DotMatrixLcdState>) {
     """
     )
 
-    /*
-    val chE = PagerChar.Alpha(PagerCode("10"), "E")
-    val chG = PagerChar.Alpha(PagerCode("27"), "G")
-    val chK = PagerChar.Alpha(PagerCode("36"), "K")
-    val chL = PagerChar.Alpha(PagerCode("37"), "L")
-    val chM = PagerChar.Alpha(PagerCode("38"), "M")
-    val chO = PagerChar.Alpha(PagerCode("30"), "O")
-    val chT = PagerChar.Alpha(PagerCode("40"), "T")
-    val chY = PagerChar.Alpha(PagerCode("50"), "Y")
-     */
+    val chE = PagerChar.Alpha(
+        PagerCode("10"), "E", """
+       11111
+       10000
+       10000
+       11110
+       10000
+       10000
+       11111
+    """
+    )
+    val chG = PagerChar.Alpha(
+        PagerCode("27"), "G", """
+       01110
+       10001
+       10000
+       10111
+       10001
+       10001
+       01111
+    """
+    )
+    val chK = PagerChar.Alpha(
+        PagerCode("36"), "K", """
+       10001
+       10010
+       10100
+       11000
+       10100
+       10010
+       10001
+    """
+    )
+    val chL = PagerChar.Alpha(
+        PagerCode("37"), "L", """
+       00000
+       00000
+       00000
+       00000
+       00000
+       00000
+       00000
+    """
+    )
+    val chM = PagerChar.Alpha(
+        PagerCode("38"), "M", """
+       00000
+       00000
+       00000
+       00000
+       00000
+       00000
+       00000
+    """
+    )
+    val chO = PagerChar.Alpha(
+        PagerCode("30"), "O", """
+       01110
+       10001
+       10001
+       10001
+       10001
+       10001
+       01110
+    """
+    )
+    val chT = PagerChar.Alpha(
+        PagerCode("40"), "T", """
+       11111
+       00100
+       00100
+       00100
+       00100
+       00100
+       00100
+    """
+    )
+    val chY = PagerChar.Alpha(
+        PagerCode("50"), "Y", """
+       10001
+       10001
+       01010
+       00100
+       00100
+       00100
+       00100
+    """
+    )
+    private val charset = setOf(emSmile, chA, chE, chG, chK, chL, chM, chO, chT, chY)
+
     fun update(text: String) {
-        dotMatrixLcdState.value.update(listOf(chA))
+        dotMatrixLcdState.update(stringToChars(text))
+    }
+
+    private fun stringToChars(text: String): List<PagerChar> {
+        return text.map {
+            charset.find { ch -> ch.char == it.toString() }
+        }.filterNotNull()
     }
 }
 
 @Composable
-private fun rememberPagerLcdState(): State<PagerLcdState> {
-    val lcdState = remember { mutableStateOf(DotMatrixLcdStateImpl()) }
-    return remember { mutableStateOf(PagerLcdState(lcdState)) }
+private fun rememberPagerLcdState(): PagerLcdState {
+    val lcdState = remember { DotMatrixLcdStateImpl() }
+    return remember { PagerLcdState(lcdState) }
 }
 
 @Composable
-fun PagerLcd() {
+fun PagerLcd(modifier: Modifier = Modifier) {
     val state = rememberPagerLcdState()
     val str1 = "TOKYO"
     val str2 = "TELEMESSAGE"
 
     LaunchedEffect(Unit) {
-        state.value.update(str1)
+        state.update(str1)
     }
-    DotMatrixLcd(state.value.dotMatrixLcdState.value)
+    DotMatrixLcd(state.dotMatrixLcdState, modifier)
 }
 
 @Stable
@@ -99,17 +187,23 @@ class DotMatrixLcdStateImpl(initialData: IntArray = intArrayOf()) : DotMatrixLcd
 
     override fun update(chars: List<PagerChar>) {
         val data = IntArray(dotWidth * dotHeight) { 0 }
-        chars.forEach {
-            val ch = chars[0]
-            for (y in 0 until ch.size.height) {
+        val charMarginWidth = 1
+
+        // 一番上のドット行ごとに文字を描画
+        // TODO 2次元的にコピーしてからシリアライズしたほうがやりやすそう
+        for (y in 0 until dotHeight) {
+            var destinationOffset = dotWidth * y
+            chars.forEachIndexed() { index, ch ->
+                if (y >= ch.size.height) return@forEachIndexed
+
                 val startIndex = ch.size.width * y
-                ch.charData.copyInto(data, dotWidth * y, startIndex, startIndex + ch.size.width)
+                val endIndex = startIndex + ch.size.width
+                ch.charData.copyInto(data, destinationOffset, startIndex, endIndex)
+                destinationOffset += ch.size.width + charMarginWidth
             }
         }
         update(data)
     }
-
-
 }
 
 @Composable
@@ -117,6 +211,17 @@ fun DotMatrixLcd(
     state: DotMatrixLcdState,
     modifier: Modifier = Modifier
 ) {
+
+    val density = LocalDensity.current
+    val dotSize = with(density) {
+        //Size(12f, 12f)
+        Size(6.dp.toPx(), 6.dp.toPx())
+    }
+    val dotMargin = with(density) {
+        // 2f
+        0.5.dp.toPx()
+    }
+
     Canvas(
         modifier = modifier
             .fillMaxSize()
@@ -131,13 +236,17 @@ fun DotMatrixLcd(
                 } else {
                     Color(0x44222244)
                 }
-                val topLeft = offset + Offset(x * 10f, y * 10f)
-                val dotSize = Size(8f, 8f)
+
+                val topLeft =
+                    offset + Offset(
+                        x * (dotSize.width + dotMargin),
+                        y * (dotSize.height + dotMargin)
+                    )
                 val rect = Rect(topLeft, dotSize)
                 if (size.toRect().contains(rect)) {
                     drawRect(
                         topLeft = topLeft,
-                        size = Size(8f, 8f),
+                        size = dotSize,
                         color = color
                     )
                 }
